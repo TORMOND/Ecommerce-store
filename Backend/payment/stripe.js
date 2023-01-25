128cb8889f0dabad267f8def1cba979af314cc90
victormonderu@gmail.com <victormonderu@gmail.com>
2023-01-25 23:29:52 +0300

    made changes on calculating payment on the stripe API

--- Backend/payment/stripe.js
+++ Backend/payment/stripe.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 const Product = require('../Schemas/storeSchemas')
+require('dotenv').config()
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require("stripe")('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe_secret_key = process.env.STRIPE_SECRET_KEY
+const stripe_publishable_key = process.env.STRIPE_PUBLISHABLE_KEY
+const stripe = require("stripe")(stripe_secret_key);
 
-const getProduct = async (req, res)=>{ 
-  const { items } = req.body;
-  const product = await Product.findById(items)
+const getProduct = async ()=>{ 
+
+  const product = await Product.findById(id)
    if(!product){
   return res.status(404).json({error: 'No such product'})
   }
@@ -20,16 +23,13 @@ const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
-
- const productPrice =  getProduct().price
- 
+  const productPrice = items.price()
   
   return productPrice;
 };
 
 const makePayment = async(req, res)=>{
   const { items } = req.body;
-
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
@@ -47,3 +47,4 @@ const makePayment = async(req, res)=>{
 
 
 module.exports = { makePayment }
+
